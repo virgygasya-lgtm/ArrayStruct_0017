3710f290b6de060377348d79a4d668664778e4e2
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-06 10:33:06 +0700

    app.cpp

---
+++ app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    // deklarasi tanpa inisialisasi
+    int datamu[5];
+    
+}
