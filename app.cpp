113a62a1d6ab5d06d79a75b4ebad650a1ba416e1
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-06 10:45:34 +0700

    app.cpp

--- app.cpp
+++ app.cpp
@@ -17,5 +17,12 @@ cout << "isi data kedua setelah dirubah : " << dataku[1] << endl;
 // mengisi data dengan perintah satu persatu
 cout << "isikan data pertama : ";
 cin >> datamu[0];
-cout << "isikan data kedua : ';
-"
+cout << "isikan data kedua : ";
+cin >> datamu[1];
+cout << "isikan data ketiga : ";
+cin >> datamu[2];
+cout << "isikan data keempat : ";
+cin >> datamu[3];
+cout << "isikan data kelima : ";
+cin >>datamu[4];
+
