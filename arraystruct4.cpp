3443ffef0e8f01a0c77a2d89ccf35a934fb57434
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-06 11:39:22 +0700

    arraystruct4.cpp

---
+++ arraystruct4.cpp
@@ -0,0 +1,39 @@
+include <iostream>
+#include <string>
+using namespace std;
+
+struct alamat Detail
+{
+    string Desa;
+    string kota;
+}
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+}
+
+int main()
+{
+    // deklarasi variabel struct
+    mahasiswa mhs;
+
+    //Mengisi data
+    cout << "isikan Data Nama : ";
+    getline(cin, mhs.nama);
+    
+    cout<< "isikan data kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "isikan Dta Desa : ";
+    getline(cin, mhs.alamat.Desa);
+    cout << "isikan Dta Usia : ";
+    cin >> mhs.umur;
+
+    cout <<"Data Mahasiswa"<< endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "kota : "<< mhs.alamat.kota << endl;
+    cout << "Desa : "<< mhs.alamat.Desa << endl;
+    cout << "usia : "<< mhs.umur << endl;
+}
