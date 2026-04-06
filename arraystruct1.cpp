945f7bfbe03689fb8ca88afb895c99b8c9158099
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-06 11:52:11 +0700

    arraystruct1.cpp

--- arraystruct1.cpp
+++ arraystruct1.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    // deklarasi array disertai inisialisasi
+     // deklarasi array disertai inisialisasi
     int dataku[5] = {10, 20, 30, 40, 50};
+
     // deklarasi tanpa inisialisasi
     int datamu[5];
 
-// cara akses (membaca atau mengambil isi array)
-cout << "isi data kedua : " << dataku[1] << endl;
-// mengisi array
-dataku[1] = 200;
-cout << "isi data kedua setelah dirubah : " << dataku[1] << endl;
+    // cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
 
-// mengisi data dengan perintah satu persatu
-cout << "isikan data pertama : ";
-cin >> datamu[0];
-cout << "isikan data kedua : ";
-cin >> datamu[1];
-cout << "isikan data ketiga : ";
-cin >> datamu[2];
-cout << "isikan data keempat : ";
-cin >> datamu[3];
-cout << "isikan data kelima : ";
-cin >>datamu[4];
+       // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
 
-//menampilkan data dengan perintah satu per satu
-cout << "data pertama : " << datamu[0] << endl;
-cout << "data kedua : " << datamu[1] << endl;
-cout << "data ketiga : " << datamu[2] << endl;
-cout << "data keempat : " << datamu[3] << endl;
-cout << "data kelima : " << datamu [4] << endl;
+     // mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+      cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+      cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
 
-//menampilkan data dengan perulangan
-for (int i = 0; i <=4; i++)
-{
-    cout << "data ke-"<<(i+1)<<(i+1) <<":"<<datamu[i]<<endl;
-}
-{
-    return 0;
+     // menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+     // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+        {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }
 }
