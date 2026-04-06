d42b2971fbab55fcb62e40303a078e46fe04ed81
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-06 11:22:47 +0700

    arraystruct3.cpp

---
+++ arraystruct3.cpp
@@ -0,0 +1,17 @@
+include <iostream>
+#include <string>
+using namespace std;
+
+struct mahasiswa
+{
+    string Desa;
+    string kota;
+}
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+}
+   
