c5d46a9812c0503021115903f4ae168636a0f131
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-06 11:28:58 +0700

    arraystruct.cpp3

--- arraystruct3.cpp
+++ arraystruct3.cpp
@@ -14,4 +14,17 @@ struct mahasiswa
     alamatDetail alamat;
     int umur;
 }
-   
+
+int main()
+{
+    // deklarasi variabel struct
+    mahasiswa mhs;
+    //Mengisi data
+    cout << "isikan Data Nama : ";
+    getline(cin, mhs.nama);
+    cout<< "isikan data kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "isikan Dta Desa : ";
+    getline(cin, mhs.alamat.Desa);
+    cout << "isikan Dta Usia : ";
+    cin >> mhs.umur;
